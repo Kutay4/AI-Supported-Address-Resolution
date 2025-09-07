a3997dd18db9912c03f89bf7410c5e5bca93882b
Kutay4 <141006414+Kutay4@users.noreply.github.com>
2025-09-07 12:26:24 +0300

    last changes on demo.py

--- demo.py
+++ demo.py
@@ -2,8 +2,9 @@ import streamlit as st
 import polars as pl
 import pandas as pd
 import numpy as np
-from utils.predict import predict_address_onnx
+from utils.predict import predict_address_onnx, predict_csv_onnx
 from utils.standardize import return_cannon
+import time
 
 model_path = "./model_files/model_quantized_onnx/model.onnx"
 tokenizer_path = "./model_files/model_quantized_onnx"
@@ -26,7 +27,6 @@ text_input = st.text_input(
 if text_input != "":
     ner_output, table = return_cannon(address=text_input)
     prediction = predict_address_onnx(text_input, model_path, tokenizer_path)
-    
     col1, col2 = st.columns(2)
     
     with col1:
@@ -55,3 +55,62 @@ if text_input != "":
     st.header("Best Matches")
     st.write("---")
     st.table(table)
+
+st.title("Inference from CSV file")
+st.subheader("Upload a CSV file and get predictions from the model.")
+
+uploaded_file = st.file_uploader(
+    "Choose a CSV file",
+    type="csv",
+    help="Upload your CSV file with address string for prediction."
+)
+
+if uploaded_file is not None:
+    try:
+        df_uploaded = pd.read_csv(uploaded_file,index_col=0)
+
+        with st.expander("Show Uploaded Dataset"):
+            st.dataframe(df_uploaded)
+
+        st.write("---")
+        if st.button("Start Inference"):
+            with st.spinner("Processing your data..."):
+                progress_bar = st.progress(0, text="Inference in progress...")
+
+                start_time = time.perf_counter()
+                df_with_predictions = predict_csv_onnx(df_uploaded, model_path, tokenizer_path, progress_bar)
+
+                end_time = time.perf_counter()
+                elapsed_time = end_time - start_time
+            
+                st.success(f"Inference completed in {elapsed_time:.4f} seconds.")
+                progress_bar = st.progress(0, text="Adress Parsing in progress...")
+
+                start_time = time.perf_counter()
+
+                rows = []
+                for t,address in enumerate(df_with_predictions["address"]):
+                    progress_percentage = (t + 1) / len(df_with_predictions)
+                    progress_bar.progress(progress_percentage, text=f"{(t+1)}%")
+                    _, table = return_cannon(address=address, return_top=1)
+                    rows.append(table['city_name','district_name','quarter_name', 'street_name'].to_dicts()[0])
+
+                ner_output_df = pd.DataFrame(rows)
+                df_with_predictions = df_with_predictions.merge(ner_output_df, left_index=True, right_index=True,)
+                end_time = time.perf_counter()
+                elapsed_time = end_time - start_time
+                st.success(f"Address Parsing completed in {elapsed_time:.4f} seconds.")
+            st.subheader("Prediction Results")
+            st.dataframe(df_with_predictions)
+
+            # --- Download Button ---
+            st.download_button(
+                label="Download Predictions as CSV",
+                data=df_with_predictions.to_csv(index=False),
+                file_name="predictions.csv",
+                mime="text/csv",
+                icon="⬇️",
+            )
+    
+    except Exception as e:
+        st.error(f"An error occurred while processing the file: {e}")
--- utils/predict.py
+++ utils/predict.py
@@ -4,7 +4,10 @@ os.environ["TOKENIZERS_PARALLELISM"] = "true"
 import onnxruntime as ort
 from transformers import AutoTokenizer
 import numpy as np
-
+import pandas as pd
+from .model_utils import AddressTestDataset
+from torch.utils.data import DataLoader
+import streamlit as st
 
 def predict_address_onnx(address, model_path, tokenizer_path):
     ort_session = ort.InferenceSession(model_path)
@@ -25,6 +28,32 @@ def predict_address_onnx(address, model_path, tokenizer_path):
     pred = logit.argmax(axis=1)[0] + 1
 
     return pred
+
+def predict_csv_onnx(csv_file, model_path, tokenizer_path, progress_bar, batch_size=4, streeamlit = True):
+
+    ort_session = ort.InferenceSession(model_path)
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+
+    test_dataset = AddressTestDataset(csv_file, tokenizer)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    preds = []
+
+    for i,batch in enumerate(test_loader):
+        ort_inputs = {
+            "input_ids": batch["input_ids"].cpu().numpy().astype(np.int64),
+            "attention_mask": batch["attention_mask"].cpu().numpy().astype(np.int64),
+        }
+        logits = ort_session.run(["logits"], ort_inputs)[0]
+        batch_preds = logits.argmax(axis=1)
+        preds.extend(batch_preds.tolist())
+        if streeamlit:
+            progress_percentage = (i + 1) / len(test_loader)
+            progress_bar.progress(progress_percentage, text=f"{(i+1)*batch_size}%")
+
+    preds = np.array(preds)
+    return pd.DataFrame({"address": csv_file["address"], "label": preds + 1})
+
 
 
 if __name__ == "__main__":
